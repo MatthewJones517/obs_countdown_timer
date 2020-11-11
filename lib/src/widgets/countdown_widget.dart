83a359be52303e83b1beb823f5127ade8d10c4a5
Matthew Jones <mail@matthewrjones.me>
2020-11-11 12:51:08 -0500

    Set up countdown widget to consume settings service

--- lib/src/widgets/countdown_widget.dart
+++ lib/src/widgets/countdown_widget.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:obs_countdown_timer/src/services/settings_service.dart';
 
 class CountdownWidget extends StatelessWidget {
   Widget build(context) {
-    return Container(
-      color: Colors.green,
-      child: Center(
-        child: Text('00:00'),
-      ),
-    );
+    return Consumer<SettingsService>(
+        builder: (context, settingsService, child) {
+      return Container(
+        color: settingsService.preferences.keyType,
+        child: Center(
+          child: Text(
+            '00:00',
+            style: GoogleFonts.getFont(
+              settingsService.preferences.googleFontName,
+              textStyle: TextStyle(
+                  color: settingsService.preferences.timerColor,
+                  fontSize: settingsService.preferences.timerFontSize),
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
