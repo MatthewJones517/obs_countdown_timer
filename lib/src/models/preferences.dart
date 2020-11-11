9509eeb57a20fe75ea5a1ef6e54bfe39e2c8e6c7
Matthew Jones <mail@matthewrjones.me>
2020-11-11 11:53:53 -0500

    Changed how luma / chroma keys are represented to make it more obvious

--- lib/src/models/preferences.dart
+++ lib/src/models/preferences.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class PreferencesModel {
   /// If set to true Chroma will be used. Otherwise Luma will be used.
-  final bool isChroma;
+  final Color keyType;
 
   /// Google Font Name. Not all Google fonts are supported.
   final String googleFontName;
@@ -21,13 +21,28 @@ class PreferencesModel {
 
   final String finishedMessage;
 
+  /// Builds with passed in settings
+  PreferencesModel(
+      {this.keyType,
+      this.googleFontName,
+      this.timerFontSize,
+      this.timerColor,
+      this.timerStrokeSize,
+      this.timerStrokeColor,
+      this.finishedMessage});
+
   /// Builds with default settings
-  PreferencesModel()
-      : isChroma = false,
+  PreferencesModel.withDefaults()
+      : keyType = KeyType.luma,
         googleFontName = 'ConcertOne',
         timerFontSize = 72,
         timerColor = Colors.white,
         timerStrokeSize = 5,
         timerStrokeColor = Colors.black,
         finishedMessage = "We'll get started shortly!";
+}
+
+class KeyType {
+  static const Color luma = Color(0xFF000000);
+  static const Color chroma = Color(0xFF00b140);
 }
