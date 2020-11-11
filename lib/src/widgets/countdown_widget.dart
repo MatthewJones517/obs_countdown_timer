6b0a15fd2c1637a8d5a105227c7ee8a287f52ce6
Matthew Jones <mail@matthewrjones.me>
2020-11-11 11:11:10 -0500

    Created placeholder code for CountdownWidget

---
+++ lib/src/widgets/countdown_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CountdownWidget extends StatelessWidget {
+  Widget build(context) {
+    return Center(
+      child: Text('00:00'),
+    );
+  }
+}
