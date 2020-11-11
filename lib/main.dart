f0121de09f0c0839ab9790f1cc9f5bb654ef48fc
Matthew Jones <mail@matthewrjones.me>
2020-11-11 11:59:23 -0500

    Added provider for settings service

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:obs_countdown_timer/src/app.dart';
+import 'package:obs_countdown_timer/src/services/settings_service.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
-  runApp(App());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => SettingsService(),
+        ),
+      ],
+      child: App(),
+    ),
+  );
 }
